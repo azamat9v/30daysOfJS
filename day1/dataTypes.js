5f757fce64e64982bf45e8281943976d8b7ffb5f
Azamat <azamatahmadov31@gmail.com>
2023-06-16 10:54:08 +0500

    day1.1

--- day1/dataTypes.js
+++ day1/dataTypes.js
@@ -1,6 +1,28 @@
-let firstName = "Azamat"
+// 3. Write a multiline comment which says, comments can make code readable, easy to reuse and informative
+// all commit // 
+/* separately commit*/
+// Welcome to 30DaysOfJavaScript//
 
-let num = 4635432;
-let text = 'The quick-brown fox jumps over-the lazy dog.'
 
-console.log(typeof null)
+
+
+//4. Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+// let text = "Azamat going to Js City"; /* string */
+// let rotmi = true; /* boolen */
+// let malumotsiz = ""; /* undefined  */
+// let = nimadur;  /*null */
+
+
+
+
+//5. Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+// let num = 42;
+// let num1 = "Azamat";
+// let num2 = true;
+// let num3 = ``;
+// let num4;
+// console.log(typeof 42);
+// console.log(typeof "Azamat");
+// console.log(typeof true);
+
+
