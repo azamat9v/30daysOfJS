fd058632e3ad75e5720461cec97345dd6d516f18
Azamat <azamatahmadov31@gmail.com>
2023-07-04 18:14:46 +0500

    fixday2

---
+++ day2/dataTypes.js
@@ -0,0 +1,105 @@
+let initial = "30 Days Of JavaScript"
+// console.log(initial);            //30 Days Of JavaScript
+
+
+// console.log(initial.length);     //21
+
+
+// console.log(initial.toUpperCase()); //30 DAYS OF JAVASCRIPT
+
+
+// console.log(initial.toLowerCase()); //30 days of javascript
+
+
+// console.log(initial.substr(0, 6)); //30 Day
+
+
+// console.log(initial.substring(0, 10)); //30 Days Of
+
+
+
+// console.log(initial.substring(3)); //Days Of JavaScript
+
+
+// console.log(initial.includes("Days"))
+// console.log(initial.includes("gays"))
+// console.log(initial.includes("Dats"))
+// console.log(initial.includes("30"))
+// console.log(initial.includes("Java")) //true false false true true
+
+
+// console.log(initial.split(" "))  
+// console.log(initial.split(",")) //'30', 'Days', 'Of', 'JavaScript'
+// console.log(initial.split('')) //'3', '0', ' ', 'D', 'a', 'y', 's', ' ', 'O', 'f',' ', 'J', 'a', 'v', 'a','S', 'c', 'r', 'i', 'p','t'
+
+
+
+// let initiall = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon"
+// console.log(initiall.split(''))
+
+// [
+//     'F', 'a', 'c', 'e', 'b', 'o', 'o', 'k',
+//     ',', ' ', 'G', 'o', 'o', 'g', 'l', 'e',
+//     ',', ' ', 'M', 'i', 'c', 'r', 'o', 's',
+//     'o', 'f', 't', ',', ' ', 'A', 'p', 'p',
+//     'l', 'e', ',', ' ', 'I', 'B', 'M', ',',
+//     ' ', 'O', 'r', 'a', 'c', 'l', 'e', ',',
+//     ' ', 'A', 'm', 'a', 'z', 'o', 'n'
+//   ]
+
+
+
+// console.log(initial.replace('JavaScript', 'Python')) //30 Days Of Python
+
+
+// console.log(initial.charAt(15))  //S
+
+
+
+// let initiall = initial.length -10
+// console.log(initial.charCodeAt(initiall)) //74
+
+
+
+
+// console.log(initial.indexOf("Days")) //3
+
+
+
+// console.log(initial.lastIndexOf("t")) //20
+
+
+
+// let chunki = "You cannot end a sentence with because because because is a conjunction"
+// console.log(chunki.indexOf('because')) //31
+
+
+
+// let chunki = "You cannot end a sentence with because because because is a conjunction"
+// console.log(chunki.lastIndexOf('because')) //47
+
+
+// let chunki = "You cannot end a sentence with because because because is a conjunction"
+// console.log(chunki.search('because')) //31
+
+
+// let initiall = " 30 Days Of JavaScript " //' 30 Days Of JavaScript '
+// console.log(initiall.trim(' ')) //30 Days Of JavaScript
+
+
+// console.log(initial.startsWith(30))//true
+// console.log(initial.startsWith(20))//false
+
+
+// console.log(initial.match(/\d+/g)) //30
+
+
+// let initiall = "My "
+// console.log(initiall.concat("30 Days Of JavaScript")) //My 30 Days Of JavaScript
+
+
+
+// console.log(initial.repeat(2)) //30 Days Of JavaScript30 Days Of JavaScript
+
+let NewTime = "The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another."
+console.log(NewTime)
