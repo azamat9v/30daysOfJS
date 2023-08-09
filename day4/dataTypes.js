1a431e0e0e174f9c4d49a716721d70320b683bfd
azamat ahmadov <azamatahmadov31@gmail.com>
2023-08-09 18:22:50 +0500

    day4

---
+++ day4/dataTypes.js
@@ -0,0 +1,27 @@
+//1
+let a=16;
+if (a>18){
+console.log("Siz prava ololisiz!")
+}else{
+    console.log("Siz hali yoshsiz")  //16<18 bo'lganiga yoshiz to`g`ri kemadi chiqadi
+}
+
+//2
+let myAge = 28;
+let youAre = 29;
+if(myAge > youAre){
+    console.log("Men kattaman")
+}else{
+    console.log("Siz katta ekansiz")
+}
+
+
+//3
+let e = 3;
+let b = 4;
+if (e > b){
+    console.log("3 katta emas 4")
+}else{
+    console.log("3 kichik 4 dan")
+}
+
