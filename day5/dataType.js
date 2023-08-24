99ca2547ae559f7e9338e0fe5db499cd15a3cf2e
azamat ahmadov <azamatahmadov31@gmail.com>
2023-08-24 16:00:49 +0500

    addDay5

---
+++ day5/dataType.js
@@ -0,0 +1,31 @@
+//1
+const arr = Array();
+console.log(arr)
+
+//2
+const arr5 = [1,2,3,4,5,6]
+console.log(arr5);
+
+
+//3
+console.log(arr5.length);
+
+//4 
+console.log(arr5[0]);
+console.log(arr5[2])
+console.log(arr5.length-1);
+
+//5
+const mixedDataTypes = ["BMw", "bugatti", "mesades", "chevrolet", "lada" , "byd"]
+console.log(mixedDataTypes.length);
+
+//6
+const itCompanies = ["Facebook", "Google", "Microsoft", "Apple", "IBM", "Oracle", "Amazon"]
+console.log(itCompanies[0])
+
+//8
+console.log(itCompanies);
+
+//9
+console.log(itCompanies.lastIndexOf);
+console.log(itCompanies[3]);
